766630e811d48f2b58e3f95cadc7ad1af47ac6eb
Dante Dam <laggycomputer@yahoo.com>
2024-04-29 16:40:19 -0700

    revise ReaderMaybeGzip to simplify types

--- src/reader.rs
+++ src/reader.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io;
-use std::io::{BufReader, Read};
+use std::io::{BufRead, BufReader, Read};
 use std::path::PathBuf;
 use std::process::exit;
 
@@ -8,9 +8,9 @@ use bio::io::fastq;
 use flate2::bufread::MultiGzDecoder;
 
 pub(crate) enum ReaderMaybeGzip {
-    GZIP(MultiGzDecoder<BufReader<File>>),
+    GZIP(BufReader<MultiGzDecoder<BufReader<File>>>),
     UNCOMPRESSED(BufReader<File>),
-    NULL(io::Empty),
+    NULL(BufReader<io::Empty>),
 }
 
 impl Read for ReaderMaybeGzip {
@@ -23,7 +23,25 @@ impl Read for ReaderMaybeGzip {
     }
 }
 
-pub(crate) fn reader_maybe_gzip(path_buf: &PathBuf) -> Result<(fastq::Reader<BufReader<ReaderMaybeGzip>>, bool), io::Error> {
+impl BufRead for ReaderMaybeGzip {
+    fn fill_buf(&mut self) -> io::Result<&[u8]> {
+        match self {
+            ReaderMaybeGzip::GZIP(backer) => backer.fill_buf(),
+            ReaderMaybeGzip::UNCOMPRESSED(backer) => backer.fill_buf(),
+            ReaderMaybeGzip::NULL(backer) => backer.fill_buf(),
+        }
+    }
+
+    fn consume(&mut self, amt: usize) {
+        match self {
+            ReaderMaybeGzip::GZIP(backer) => backer.consume(amt),
+            ReaderMaybeGzip::UNCOMPRESSED(backer) => backer.consume(amt),
+            ReaderMaybeGzip::NULL(backer) => backer.consume(amt),
+        }
+    }
+}
+
+pub(crate) fn reader_maybe_gzip(path_buf: &PathBuf) -> Result<(fastq::Reader<ReaderMaybeGzip>, bool), io::Error> {
     let mut file = File::open(path_buf)?;
     let mut magic = [0; 2];
     file.read(&mut magic[..])?;
@@ -31,13 +49,13 @@ pub(crate) fn reader_maybe_gzip(path_buf: &PathBuf) -> Result<(fastq::Reader<Buf
     let reopen = BufReader::new(File::open(path_buf)?);
 
     if magic.eq(&[0x1f, 0x8b]) {
-        Ok((fastq::Reader::from_bufread(BufReader::new(ReaderMaybeGzip::GZIP(MultiGzDecoder::new(reopen)))), true))
+        Ok((fastq::Reader::from_bufread(ReaderMaybeGzip::GZIP(BufReader::new(MultiGzDecoder::new(reopen)))), true))
     } else {
-        Ok((fastq::Reader::from_bufread(BufReader::new(ReaderMaybeGzip::UNCOMPRESSED(reopen))), false))
+        Ok((fastq::Reader::from_bufread(ReaderMaybeGzip::UNCOMPRESSED(reopen)), false))
     }
 }
 
-fn reader_from_path(path_buf: &PathBuf, silent: bool) -> fastq::Reader<BufReader<ReaderMaybeGzip>> {
+fn reader_from_path(path_buf: &PathBuf, silent: bool) -> fastq::Reader<ReaderMaybeGzip> {
     match reader_maybe_gzip(path_buf) {
         Ok((result, was_compressed)) => {
             if was_compressed && !silent { eprintln!("info: parsing {} as a gzip", path_buf.display()) }
@@ -51,7 +69,6 @@ fn reader_from_path(path_buf: &PathBuf, silent: bool) -> fastq::Reader<BufReader
 }
 
 pub(crate) fn make_reader_pair(input_paths: (&PathBuf, &PathBuf), silent: bool)
-                               -> (fastq::Reader<BufReader<ReaderMaybeGzip>>,
-                                   fastq::Reader<BufReader<ReaderMaybeGzip>>) {
+                               -> (fastq::Reader<ReaderMaybeGzip>, fastq::Reader<ReaderMaybeGzip>) {
     (reader_from_path(input_paths.0, silent), reader_from_path(input_paths.1, silent))
 }
